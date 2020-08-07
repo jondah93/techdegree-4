df66ba449025929d06c5d5dc342089f850ba71e5
jondah93 <65925418+jondah93@users.noreply.github.com>
2020-08-07 22:02:42 +0200

    added backup db method and some final refactoring/tweaks

--- app.py
+++ app.py
@@ -1,12 +1,13 @@
 import datetime
 from collections import OrderedDict
-import sys
 import os
 import csv
 import re
+from decimal import Decimal
 
 from peewee import *
 from playhouse.shortcuts import model_to_dict
+
 
 db = SqliteDatabase('inventory.db')
 
@@ -14,13 +15,14 @@ db = SqliteDatabase('inventory.db')
 class Product(Model):
     product_id = PrimaryKeyField()
     product_name = TextField(unique=True)
-    product_quantity = IntegerField()
+    product_quantity = IntegerField(default=0)
     product_price = IntegerField()
     date_updated = TimestampField(default=datetime.datetime.now)
 
 
     class Meta:
         database = db
+
 
 def clear():
     """Clear the terminal."""
@@ -29,52 +31,6 @@ def clear():
 def initialize():
     db.connect()
     db.create_tables([Product], safe=True)
-
-def take_product_input():
-    product_name = input('Enter product name >>> ').strip()
-    product_price = False # So user won't have to re-enter price if they get ValueError at quantity-prompt
-    while True:
-        try:
-            if not product_price:
-                product_price = int(input('Enter product price (cents) >>> '))
-            product_quantity = int(input('Enter product quantity >>> '))
-
-            return {'product_name': product_name,
-                    'product_price': product_price,
-                    'product_quantity': product_quantity,
-                    'date_updated': datetime.datetime.now(),
-                    }
-        except ValueError as err:
-            print(('Invalid input' + '\nError: {}').format(err))
-
-def add_entry(product=None, verbose=True):
-    """Adds a product to the database."""
-    _product = take_product_input() if product == None else product
-    try:
-        Product.create(**_product)
-    except IntegrityError:
-        if Product.select().where(
-            (Product.product_name == _product['product_name']) &
-            (Product.date_updated <= _product['date_updated'])
-        ).exists():
-            Product.update({
-                Product.product_price: _product['product_price'],
-                Product.product_quantity: _product['product_quantity'],
-                Product.date_updated: _product['date_updated'],
-            }).where(
-                (Product.product_name == _product['product_name']) &
-                (Product.date_updated <= _product['date_updated'])
-            ).execute()
-
-            if verbose:
-                print(('Product "{}" already exists. Fields updated accordingly.').format(
-                    _product['product_name']
-                ))
-        else:
-            if verbose:
-                print(('A more recent version of "{}" is already stored in the database.').format(
-                    _product['product_name']
-                ))
 
 def view_entries(specific=False, verbose=False):
     """Prints all the items in the database if not a specific product is passed as an argument."""
@@ -88,32 +44,90 @@ def view_entries(specific=False, verbose=False):
             print(model_to_dict(product))
         else:
             for k, v in model_to_dict(product).items():
-                if k == 'product_price':
-                    v = str(('${}').format(v/100))
+                if k == 'product_price': # Print the price in dollars.
+                    v = ('${}').format(Decimal(str(Decimal(v)/100)).quantize(Decimal('.00')))
                 print(('{}: {}').format(k, v))
 
-def view_by_id(id=None):
-    """View a product (product id required)."""
-    if id is None:
-        while True:
-            try:
-                id = int(input('Enter a product id >>> '))
-                product = Product.select().where(Product.product_id == id).get()
-                view_entries(specific=product, verbose=True)
+def take_product_input():
+    """Takes user input and returns a dict that can be stored in db."""
+    product_name = input('Enter product name >>> ').strip()
+    product_price = False # So user won't have to re-enter price if they get ValueError at quantity-prompt
+    while True:
+        try:
+            if not product_price:
+                product_price = int(input('Enter product price (cents) >>> '))
+            product_quantity = int(input('Enter product quantity >>> '))
 
-                break
-            except (DoesNotExist, ValueError) as err:
-                print(('Product not found.\nError: {}').format(err))
+            product = {'product_name': product_name,
+                    'product_price': product_price,
+                    'product_quantity': product_quantity,
+                    'date_updated': datetime.datetime.now(),
+                    }
+            return product
+        except ValueError as err:
+            print(('Invalid input' + '\nError: {}').format(err))
+
+def add_entry(product=None, verbose=True):
+    """Add a product to the database."""
+    _product = take_product_input() if product == None else product
+    try:
+        Product.create(**_product)
+    except IntegrityError:
+        try:
+            Product.update({
+                Product.product_price: _product['product_price'],
+                Product.product_quantity: _product['product_quantity'],
+                Product.date_updated: _product['date_updated'],
+            }).where(
+                (Product.product_name == _product['product_name']) &
+                (Product.date_updated <= _product['date_updated'])
+            ).execute()
+
+            if verbose:
+                print(('\nProduct "{}" already exists. Fields updated accordingly.').format(
+                    _product['product_name']
+                ))
+        except DoesNotExist:
+            if verbose:
+                print(('\nA more recent version of "{}" is already stored in the database.').format(
+                    _product['product_name']
+                ))
+        finally:
+            if verbose:
+                input('\nPress ENTER to continue...')
+
+def view_by_id():
+    """View a product (product id required)."""
+    while True:
+        clear()
+        try:
+            id = int(input('Enter a product id >>> '))
+            product = Product.select().where(Product.product_id == id).get()
+            clear()
+            view_entries(specific=product, verbose=True)
+
+            break
+        except (DoesNotExist, ValueError):
+            print('\nPRODUCT NOT FOUND')
+        finally:
+            input('\nPress ENTER to continue...')
 
 def export_db():
     """Exports database to csv-file."""
-    pass
+    products = Product.select().order_by(Product.date_updated.desc())
+    with open('backup.csv', 'w') as f:
+        writer = csv.writer(f)
+        writer.writerow(Product._meta.sorted_field_names)
+        writer.writerows(products.tuples())
+
+        input(('DB exported to {}.\n\nPress ENTER to continue...').format(
+            (os.getcwd() + '/backup.csv')
+        ))
 
 def dict_data_cleanup(list_of_dicts):
     """Cleans up data in line with requirements."""
     for item in list_of_dicts:
-        item['product_name'] = item['product_name'].strip(),
-        item['product_price'] = int(re.sub('[^0-9]', '', item['product_price']))
+        item['product_price'] = int(float(re.sub('[^0-9.]', '', item['product_price']))) * 100
         item['product_quantity'] = int(item['product_quantity'])
         item['date_updated'] = datetime.datetime.strptime(item['date_updated'], '%m/%d/%Y')
 
@@ -138,13 +152,13 @@ def menu_loop():
     choice = None
 
     while choice != 'q':
-        print('Enter "q" to quit.')
+        clear()
         for key, value in menu.items():
             print('{}) {}'.format(key, value.__doc__))
-        choice = input('Action: ').lower().strip()
+        choice = input('\nEnter an option from the menu or "q" to quit >>> ').lower().strip()
+        clear()
 
         if choice in menu:
-            clear()
             menu[choice]()
 
 menu = OrderedDict([
